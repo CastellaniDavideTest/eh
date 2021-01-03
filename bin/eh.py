d16ac0bafc78efb6e01497d186a4508f34d9cfa2
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 16:42:01 +0100

    Created bin/eh.py

---
+++ bin/eh.py
@@ -0,0 +1,19 @@
+"""eh
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-3"
+
+class eh:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(eh.eh())
+	
+	def eh():
+		"""eh first funtion
+		"""
+		return "eh" #for the test (see test_eh.py)
+		
+if __name__ == "__main__":
+	eh()
